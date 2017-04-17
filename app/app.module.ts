ad12534b4bfd378e32c9b59821f3255d9ac1f15a
Rory Carroll <Rory Carroll>
2017-04-17 19:33:47 +0100

    task card and weather service combined

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,19 +2,29 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'; //allows you to run angular in a browser, it can be run on a mobile, desktop, etc...
 import {FormsModule} from '@angular/forms';
+import {JsonpModule, HttpModule} from '@angular/http';
 
 //Components
 import { AppComponent} from './app.component';
 import { CardComponent} from './card/card.component';
+import { WeatherComponent} from './weather-widget/component/weather.component';
+
+import { SpeedUnitPipe} from './weather-widget/pipe/speed-unit.pipe';
+import { TempUnitPipe} from './weather-widget/pipe/temp-unit.pipe';
 
 @NgModule({
     imports:[
         BrowserModule,
-        FormsModule
+        FormsModule,
+        JsonpModule,
+        HttpModule
         ],
     declarations: [
         AppComponent,
-        CardComponent
+        CardComponent,
+        WeatherComponent,
+        SpeedUnitPipe,
+        TempUnitPipe
         ],
     bootstrap: [AppComponent]
 })
